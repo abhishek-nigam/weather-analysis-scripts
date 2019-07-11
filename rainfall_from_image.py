2e677dc8913be93f5eac07b484377288f9790cd2
Abhishek Nigam <abhisheknigam1996@gmail.com>
2019-07-11 09:33:39 +0530

    Averaging rainfall from nearby observations points for each position/place

--- rainfall_from_image.py
+++ rainfall_from_image.py
@@ -7,6 +7,7 @@
 
 import sys
 from datetime import date
+from functools import reduce
 
 import numpy
 import xlsxwriter
@@ -28,33 +29,41 @@ image = cv2.cvtColor(numpy.array(pil_image), cv2.COLOR_RGB2BGR)
 # NOTE: If using conventional map (x,y) coordinates,
 # NOTE: write them as (y,x) here
 places = {
-    "Palam": (362, 328),
+    "Palam": (363, 326),
     "Lodhi Road": (361, 358),
     "Faridabad": (408, 382),
-    "Ridge": (404, 349),
-    "Ayanagar": (393, 317),
-    "Mungeshpur": (300, 305),
-    "Najafgarh": (357, 298)
+    "Ridge": (332, 365),
+    "Ayanagar": (391, 340),
+    "Najafgarh": (354, 305),
+    "Ghaziabad": (339, 414),
+    "Noida": (376, 397),
+    "Gr. Noida": (383, 419),
+    "Meerut": (254, 473),
+    "Baghpat": (267, 359),
+    "Bulandshahar": (408, 507),
+    "Muzaffarnagar": (124, 473),
+    "Gurugram": (395, 314),
+    "Panipat": (146, 301)
 }
 
 
-# Maps color values in RGB format to rainfall
+# Maps color values in RGB format to rainfall range
 rgb_to_rainfall = {
-    (38, 38, 38): '93 - 100 mm',
-    (41, 41, 41): '87 - 93 mm',
-    (42, 42, 42): '80 - 87 mm',
-    (43, 43, 43): '74 - 80 mm',
-    (44, 44, 44): '67 - 74 mm',
-    (40, 40, 40): '60 - 67 mm',
-    (45, 45, 45): '54 - 60 mm',
-    (23, 23, 23): '47 - 54 mm',
-    (16, 16, 16): '41 - 47 mm',
-    (5, 5, 5): '34 - 41 mm',
-    (4, 4, 4): '27 - 34 mm',
-    (3, 3, 3): '21 - 27 mm',
-    (2, 2, 2): '14 - 21 mm',
-    (1, 1, 1): '7.6 - 14 mm',
-    (6, 6, 6): '1.0 - 7.6 mm'
+    (38, 38, 38): (93, 100),
+    (41, 41, 41): (87,93),
+    (42, 42, 42): (80,87),
+    (43, 43, 43): (74,80),
+    (44, 44, 44): (67,74),
+    (40, 40, 40): (60,67),
+    (45, 45, 45): (54,60),
+    (23, 23, 23): (47,54),
+    (16, 16, 16): (41,47),
+    (5, 5, 5): (34,41),
+    (4, 4, 4): (27,34),
+    (3, 3, 3): (21,27),
+    (2, 2, 2): (14,21),
+    (1, 1, 1): (7.6,14),
+    (6, 6, 6): (1.0,7.6)
 }
 
 
@@ -63,11 +72,40 @@ rgb_to_rainfall = {
 result = []
 
 for place, pos in places.items():
-    # Open CV stores color values in BGR format
-    b, g, r = image[pos[0], pos[1]]
-    # Rearranging color values, and obtaining rainfall
-    rainfall = rgb_to_rainfall.get((r, g, b), "No rainfall")
-    result.append((place, rainfall))
+    # distance of observation points from position
+    dist = 3
+
+    # list of observation points around position
+    points = []
+    for i in range(-dist, dist + 1, dist):
+        for j in range(-dist, dist + 1, dist):
+            points.append((pos[0] + i, pos[1] + j))
+    
+    # list of RGB values at observation points
+    rgb_values = []
+    for point in points:
+        # OpenCV stores color values in BGR format
+        b, g, r = image[point[0], point[1]]
+        rgb_values.append((r ,g, b))
+    
+    # print(rgb_values)
+
+    # list of non-zero rain values (middle value of range)
+    valid_rain_values = []
+    for value in rgb_values:
+        rain_low, rain_high = rgb_to_rainfall.get(value, (0, 0))
+        rain_middle = (rain_low + rain_high) / 2
+        if rain_middle != 0:
+            valid_rain_values.append(rain_middle)
+
+    # print(len(valid_rain_values))
+    
+    if len(valid_rain_values) == 0:
+        result.append((place, "No rainfall"))
+    else:
+        rain_sum = reduce(lambda val1, val2: val1 + val2, valid_rain_values)
+        rain_avg =  rain_sum / len(valid_rain_values)
+        result.append((place, f'{"{0:.2f}".format(rain_avg - 2)} to {"{0:.2f}".format(rain_avg + 2)} mm rainfall'))
 
 
 # Write result in Workbook
